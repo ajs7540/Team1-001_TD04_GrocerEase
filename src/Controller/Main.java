094bcda62b65f18cf06caecee14cc57f728cde66
amalsabirov <ajs7540@psu.edu>
2020-11-17 15:33:22 -0500

    added rickeys and johns name to main.java....just missing johns github user

--- src/Controller/Main.java
+++ src/Controller/Main.java
@@ -19,8 +19,10 @@ public class Main {
     System.out.println("Team Number: 01");
     System.out.println("Aaron Coccagna (email: azc5793@psu.edu; github: newpolygons)");
     System.out.println("Amal Sabirov (email: ajs7540@psu.edu; github: ajs7540)");
-    System.out.println("Course: IST 261");
-    System.out.println("Assignment: TD04");
+    System.out.println("Rickey Patel (email: rpp5069@psu.edu; github: rpp5069)");
+    System.out.println("John Meskill (email: jpm6210@psu.edu; github: )");
+    System.out.println("Course:IST 261");
+    System.out.println("Assignment:TD04");
   }
 
   public static void main(String[] args) {
