601fee099af666151f2706a540a4495cfa7a4371
johnh2352 <johnmeskill275@gmail.com>
2020-11-17 17:05:56 -0500

    TODO 6 is Completed. I may require help with finishing TODO 5 but we will see.

--- src/Controller/AddProducts.java
+++ src/Controller/AddProducts.java
@@ -1,16 +1,35 @@
 package Controller;
 
+import Model.Product;
 import Model.ShoppingList;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class AddProducts {
   // TODO 08 - AddProducts: Make a debug log entry for each item added (See GrocerEase.log sample in assignment)
 
+
+  private static final Logger logger = LogManager.getRootLogger();
+
   public static void addProducts(ShoppingList shoppingList) {
     // TODO 05 - Write Controller.AddProducts.addProducts (Hint: this can be done in 6 lines)
+    //John Meskill || email: jpm6210@psu.edu || Github : johnh2352
+    while (true) {
+      Product product = ChooseProduct.chooseProduct(shoppingList, p -> p.getName());
+      logger.log(Level.DEBUG, "Added item " + product.getName() + "to list" + shoppingList.getName());
+      if (product == null) {
+        break;
+      }
 
-    /*
-     * Repeatedly have the user add new products until they're done
-     */
+      EditProducts.editOneProduct(product);
+      shoppingList.addProduct(product);
+      //THIS METHOD IS INCOMPLETE
 
+      /*
+       * Repeatedly have the user add new products until they're done
+       */
+
+    }
   }
 }
--- src/Controller/EditProducts.java
+++ src/Controller/EditProducts.java
@@ -3,10 +3,14 @@ package Controller;
 import Model.Obj;
 import Model.Product;
 import Model.ShoppingList;
+import Model.UnitOfMeasure;
 import View.IOHelper;
 import edu.psu.consolemenu.Menu;
 import edu.psu.consolemenu.MenuChoice;
 import edu.psu.consolemenu.MenuDisplay;
+
+import java.util.Scanner;
+
 
 public class EditProducts {
   public static void editProducts(ShoppingList shoppingList) {
@@ -17,11 +21,18 @@ public class EditProducts {
   public static void editOneProduct(Product product) {
     Menu menu = new Menu("Edit the Values");
     MenuChoice mcEditName = menu.addMenuChoice("");
-    // TODO 06 - Controller.EditProducts.editOneProduct: handle quantity, uom, and price
+    MenuChoice mcEditQuantity = menu.addMenuChoice("");
+    MenuChoice mcEditUOM = menu.addMenuChoice("");
+    MenuChoice mcEditPrice = menu.addMenuChoice("");
+    // TODO 06 - Controller.EditProducts.editOneProduct: handle quantity, uom, and price(FINISHED)
+    //John Meskill || email: jpm6210@psu.edu || Github : johnh2352
 
     MenuDisplay menuDisplay = new MenuDisplay(menu);
     while (true) {
       mcEditName.setText("Name: " + Obj.coalesce(product.getName(), "<not set>"));
+      mcEditQuantity.setText("Quantity: " + Obj.coalesce(product.getQuantity(), "<0.0>"));
+      mcEditUOM.setText("UOM: " + Obj.coalesce(product.getUom(), "<not set>"));
+      mcEditPrice.setText("Price: " + Obj.coalesce(product.getPrice(), "<0.00>"));
 
       MenuChoice menuChoice = menuDisplay.displayAndChoose();
       if (menuChoice == menu.getMenuChoiceQuit()) {
@@ -31,7 +42,38 @@ public class EditProducts {
       if (menuChoice == mcEditName) {
         product.setName(IOHelper.readNonBlankStringFromKeyboard("Name"));
       }
+      if (menuChoice == mcEditQuantity) {
+        product.setQuantity(IOHelper.userInputDouble("Quantity"));
+      }
+      if (menuChoice == mcEditUOM) {
+        Scanner choiceInput = new Scanner(System.in);
+        System.out.print("UOM (EA/GAL/LB/BAG/BOX/CARTON) : ");
+        String choice = choiceInput.nextLine();
+        if(choice.equals("EA")){
+          product.setUom(UnitOfMeasure.EA);
+        }
+        else if(choice.equals("GAL")){
+          product.setUom(UnitOfMeasure.GAL);
+        }
+        else if(choice.equals("LB")){
+          product.setUom(UnitOfMeasure.LB);
+        }
+        else if(choice.equals("BAG")){
+          product.setUom(UnitOfMeasure.BAG);
+        }
+        else if(choice.equals("BOX")){
+          product.setUom(UnitOfMeasure.BOX);
+        }
+        else if(choice.equals("CARTON")){
+          product.setUom(UnitOfMeasure.CARTON);
+        }
+        else{
+          System.out.println("Enter a valid value!");
+        }
+      }
+      if (menuChoice == mcEditPrice) {
+        product.setPrice(IOHelper.userInputDouble("Price"));
+      }
     }
-
   }
 }
